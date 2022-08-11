0c82b0b7f24bc676a05d873def6bb002e1b0f2de
mohamedabdifitah <learntecnology30@gmail.com>
2022-08-11 10:49:15 +0300

    added new feautures

--- web/src/home/home.js
+++ web/src/home/home.js
@@ -1,44 +1,93 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from "next/link"
+import { useEffect, useState } from 'react'
 import { BsInstagram,BsGithub } from "react-icons/bs"
 import { SiFiverr,SiUpwork } from "react-icons/si"
 //import ProfileImg from "../../public/images/IMG-20220529-WA0036.jpg"
 const Intro = () =>{
-  
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const period = 2000;
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
   return (
     <section className="intro">
-     <div className="introduction">
-      <p className="intro-text"> Asc My Name is Moh'ed Abdifitah Ahmed</p>
-      <div className='contact_link'>
-        <button >
-          Hire Me 
-        </button>
+      <div className='navbars'>
+        
+        <nav className='welcome'>
+          <h1> welcome to my portofolio</h1> 
+        </nav>
+        <nav className='quick_link'>
         <button>
-          Download resume
-        </button>
+                <BsGithub fontSize={"20px"} />
+              </button>
+              <button>
+                <BsInstagram fontSize={"20px"} />
+              </button>
+              <button>
+                <SiUpwork fontSize={"20px"} />
+              </button>
+              <button>
+                <SiFiverr fontSize={"40px"} />
+              </button>
+        </nav>
       </div>
-      
-      
+     <div className="introduction">
 
-    
-      
-      
+      {/* <p className="intro-text"> Asc My Name is Moh'ed Abdifitah Ahmed</p>     */}
+        <div className='intro_body'>
+          
+          <div className={"animate__animated animate__fadeIn"}>
+                
+                  <h1>{`Hi! I'm Moh'ed Abdifitah and im`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          </div>
+        </div>
+      <div className='contact_link'>
+          <button >
+            Hire Me 
+          </button>
+          <button>
+            Download resume
+          </button>
+        </div>
      </div>
-      <div className='quick_link'>
-          <button>
-            <BsGithub />
-          </button>
-          <button>
-            <BsInstagram />
-          </button>
-          <button>
-            <SiUpwork />
-          </button>
-          <button>
-            <SiFiverr />
-          </button>
-      </div>
     {/* <img src="images/IMG-20220502-WA0048.jpg"/>
     */}
     </section>
--- web/src/pages/footer.js
+++ web/src/pages/footer.js
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeIcon from '@mui/icons-material/Home';
 import WorkIcon from '@mui/icons-material/Work';
 import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 import PersonIcon from '@mui/icons-material/Person';
 import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
 import InfoIcon from '@mui/icons-material/Info';
 import TimelineIcon from '@mui/icons-material/Timeline';
 import ContactPageIcon from '@mui/icons-material/ContactPage';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PermContactCalendarOutlinedIcon from '@mui/icons-material/PermContactCalendarOutlined';
+import {MdWork} from "react-icons/md"
+import WorkOutlinedIcon from '@mui/icons-material/WorkOutlined';
 import Link from "next/link"
+import HomeOutlined from "@mui/icons-material/HomeOutlined";
+
+const Tabs = [
+  
+  {
+    name:"Home",
+    on:HomeIcon,
+    off:HomeOutlined
+
+  },
+  {
+    name:"services",
+    on:WorkIcon,
+    off:WorkOutlineOutlinedIcon
+  },
+  {
+    name:'about',
+    on:TimelineIcon,
+    off:InfoIcon
+  },
+  {
+    name:"contact",
+    on:PersonIcon,
+    off:PermContactCalendarOutlinedIcon
+  }
+
+]
 const Footer = () =>{
+  const [selected,setSelected] = useState(0)
+
   return (
-    <footer className="footer">
-     <Link href="#intro">
+    <footer className="footer"> 
+      { 
+        Tabs.map((Tab,index) => {
+          return(
+            <Link href="#intro">
+              <button className="icons 1"  onClick={() =>{
+
+                setSelected(index)
+              }
+                 
+              
+            }>
+                {index==selected?<Tab.on sx={{
+                color:"white"
+                
+                }}/>:<Tab.off sx={{
+                  color:"white"
+                  
+                  }}/>
+                }
+                </button>
+            </Link>
+          )
+        })
+      }
+     {/*<Link href="#intro">
       <button className="icons 1">
-      <HomeIcon sx={{
+      <HomeOutlined sx={{
         color:"white"
       }} />
       </button>
      </Link>
      <Link href="#services">
      <button className="icons 2">
-      <WorkOutlineIcon sx={{
+      <WorkOutlinedIcon sx={{
         color:"white"
       }} />
       </button>
@@ -39,6 +97,7 @@ const Footer = () =>{
       }} />
       </button>
      </Link>
+    */}
     </footer>
   )
 }
